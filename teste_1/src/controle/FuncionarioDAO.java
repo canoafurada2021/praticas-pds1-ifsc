a0591cdebbadc0bdb47ed46ff05faf79bb08c432
Patriciaaaaa <patricia.c2005@aluno.ifsc.edu.br>
2023-02-14 09:09:19 -0300

    14/02

--- teste_1/src/controle/FuncionarioDAO.java
+++ teste_1/src/controle/FuncionarioDAO.java
@@ -6,23 +6,31 @@ import modelo.Funcionario;
 
 public class FuncionarioDAO {
 
-	
 	private static ArrayList<Funcionario> listaFuncionarios;
+private static FuncionarioDAO instancia;
+	
+	public static FuncionarioDAO getInstance() {
+		if(instancia == null) {
+			instancia = new FuncionarioDAO();
+			listaFuncionarios = new ArrayList<>();
+		}
+		return instancia;
+	}
 	
 	
 	
 	public FuncionarioDAO() {
 		listaFuncionarios = new ArrayList<>();
 	}
-	
+
 	public boolean inserir(Funcionario func) {
 		listaFuncionarios.add(func);
-		
+
 		return false;
 	}
-	
-	public ArrayList<Funcionario> listar(){
+
+	public ArrayList<Funcionario> listar() {
 		return this.listaFuncionarios;
 	}
-	
+
 }
--- teste_1/src/visao/JanelaCadastroPessoa.java
+++ teste_1/src/visao/JanelaCadastroPessoa.java
@@ -5,18 +5,27 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import controle.FuncionarioDAO;
+import modelo.Funcionario;
+
 import javax.swing.JTextField;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Color;
 import javax.swing.SwingConstants;
 import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class JanelaCadastroPessoa extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField txtNomeFuncionario;
 	private JLabel lblNewLabel_1;
-	private JTextField textField;
+	private JTextField txtCpf;
 
 	/**
 	 * Launch the application.
@@ -40,33 +49,66 @@ public class JanelaCadastroPessoa extends JFrame {
 	public JanelaCadastroPessoa() {
 		setBackground(new Color(177, 245, 232));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 443, 316);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(213, 170, 255));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		lblNewLabel_1 = new JLabel("Nome:");
 		lblNewLabel_1.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
 		lblNewLabel_1.setBounds(77, 71, 46, 14);
 		contentPane.add(lblNewLabel_1);
-		
-		textField = new JTextField();
-		textField.setBounds(269, 64, 108, 29);
-		contentPane.add(textField);
-		textField.setColumns(10);
-		
+
+		txtCpf = new JTextField();
+		txtCpf.setBounds(154, 111, 123, 29);
+		contentPane.add(txtCpf);
+		txtCpf.setColumns(10);
+
 		JLabel lblNewLabel = new JLabel("CPF:");
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblNewLabel.setBounds(93, 115, 62, 17);
 		contentPane.add(lblNewLabel);
-		
+
 		txtNomeFuncionario = new JTextField();
-		txtNomeFuncionario.setBounds(154, 64, 111, 29);
+		txtNomeFuncionario.setBounds(154, 64, 123, 29);
 		contentPane.add(txtNomeFuncionario);
 		txtNomeFuncionario.setColumns(10);
-	}
 
+		JButton btnSalvar = new JButton("Enviar");
+		btnSalvar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				// Pega as variáveis inseridas no campo de texto
+				// e armazena em variaveis para utilização
+				String nome = txtNomeFuncionario.getText();
+				String cpf = txtCpf.getText();
+
+				
+				//melhorar validação para que erros tipo:
+				//adicionar funcionario mesmo com campos vazios não ocorra
+				
+				if (nome.isEmpty()) {
+					JOptionPane.showMessageDialog(null, "Nenhum nome preenchido!");
+				}
+
+				if (cpf.isEmpty()) {
+					JOptionPane.showMessageDialog(null, "Nenhum CPF preenchido!");
+				}
+
+				Funcionario func = new Funcionario();
+				func.setNome(nome);
+				func.setCpf(Long.valueOf(cpf));
+				
+				
+				FuncionarioDAO banquinhozinho = new FuncionarioDAO();
+				
+				
+			}
+		});
+		btnSalvar.setBounds(172, 165, 89, 23);
+		contentPane.add(btnSalvar);
+	}
 }
